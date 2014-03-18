85ee2e2540bc824137e0b0e7dc8cc70f0034f4d6
Chris Black <chris@bodger.org>
2014-03-18 12:36:40 -0500

    Rewrite gitxlscmp: Let Git handle temp files
    gitxlscmp.sh is now intended to be called by `git diff` instead of as a
    freestanding script.

--- gitxlscmp.sh
+++ gitxlscmp.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-FN=`basename "$1"`
-TMPFILE=`mktemp -t gitxlscmp."${FN}"` || exit 1
-git show HEAD:"$1" > "$TMPFILE"
-open -a "Microsoft Excel" "$TMPFILE" "$1" 
+echo "Ugh, Excel binaries. Here, compare them yourself."
 
-# Want to delete $TMPFILE once open in Excel,
-# but $(open) returns immediately, so check for ourselves
-until lsof -a -c "Microsoft Excel" "$TMPFILE" > /dev/null; do
-	sleep 0.5
+open -a "Microsoft Excel" "$2"
+open -a "Microsoft Excel" "$5" 
+
+# Git will delete $2 on exit (it's a tempfile),
+# but $(open) returns immediately without waiting for Excel to finish launching.
+# Therefore, stall until we know Excel has the file.
+until lsof -a -c "Microsoft Excel" "$2" > /dev/null; do
+ 	sleep 0.5
 done
-rm "$TMPFILE"
